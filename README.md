a35d25bbae529add9dc1f3e83796b1248db76736
Gabriel Cardoso Luiz <gabriel.cardoso010@gmail.com>
2020-04-09 16:11:49 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# PortsScanner
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `ports_scanner` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:ports_scanner, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/ports_scanner](https://hexdocs.pm/ports_scanner).
+
---
+++ mix.exs
@@ -0,0 +1,43 @@
+defmodule PortsScanner.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :ports_scanner,
+      version: "0.1.0",
+      elixir: "~> 1.9",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      escript: escript(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
+    ]
+  end
+
+  defp escript do
+    [main_module: PortsScanner.CLI]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {PortsScanner.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.3"},
+      {:excoveralls, "~> 0.11", only: :test}
+    ]
+  end
+end
